af0714f3394aeda1ed4a79e877f8d502338f4dd7
rspacerr <61034617+rspacerr@users.noreply.github.com>
2025-08-15 18:36:35 -0400

    fix: server sessions

--- server/main.py
+++ server/main.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, session
 from dotenv import load_dotenv
+from article import Article
 import wikipediaapi
 import random
 import os
 import re
+import secrets
 
 load_dotenv()
 app = Flask(__name__)
+app.config.update(
+   SESSION_PERMANENT=False, 
+   SESSION_TYPE="filesystem",
+   SESSION_USE_SIGNER=True
+)
+
+# sessions key
+app.secret_key = secrets.token_hex(16)
 
 # env
 USER_AGENT = os.getenv("USER_AGENT")
@@ -19,20 +29,17 @@ wiki = wikipediaapi.Wikipedia(user_agent=USER_AGENT, language='en')
 people_page = wiki.page("Wikipedia:Vital_articles/Level/4/People")
 links = people_page.links
 keys = [key for key in links.keys() if "Wikipedia:" not in links[key].title]
-random_person = None
-title = None
-
-def choose_article():
-    global random_person, title
-    random_person = wiki.page(keys[random.randint(0, len(keys))])
-    title = random_person.title
-    print("PAGE TITLE:")
-    print(title)
-    print()
-    get_section_titles()
+article_sessions = {} 
+tokens = {}
+sections = []
 
 def redact_info(input: str) -> str:
     """ Attempts to remove revealing information from input string """
+    if 'token' not in session:
+        # invalid session
+        pass
+
+    title = session['token'].get_title()
     tokens = title.split(" ");
     last_token = tokens[len(tokens)-1]
     censored = input.replace(title, "⬛"*len(title))
@@ -50,24 +57,40 @@ def redact_info(input: str) -> str:
         censored = censored.replace(last_token, "⬛"*len(last_token))
     return censored
 
-def get_section_titles():
-    if random_person == None:
-        choose_article()
-    else:
-        for section in random_person.sections:
-            if len(section.text) > 0:
-                print("TITLE:")
-                print(redact_info(section.title))
-                print()
-                print("FULL TEXT:")
-                print(redact_info(section.text))
-                print()
+@app.route("/")
+async def session_init():
+    if 'token' not in session:
+        session['token'] = secrets.token_urlsafe(32)
+        print("token not in session")
+    if session['token'] not in article_sessions:
+        await choose_article()
+    return f"Your token is {session['token']}, and the article is {article_sessions[session['token']].title}", 201
+
+async def choose_article():
+    person = wiki.page(keys[random.randint(0, len(keys))])
+    sections = [sec for sec in person.sections if len(sec.text) > 0 and sec.title != "Further reading" and sec.title != "See also" and sec.title != "External links"]
+    article_sessions[session['token']] = Article(person, person.title, sections)
+    return "", 201
+
+@app.route("/api/section")
+def get_section():
+    if 'token' not in session:
+        # invalid session, force reload page
+        pass
+    rand = random.randint(0, len(sections))
+    section = sections[rand]
+    print(jsonify({
+        "title": redact_info(section.title),
+        "text": redact_info(section.text)
+    }))
+    return jsonify({
+        "title": redact_info(section.title),
+        "text": redact_info(section.text)
+    }), 201
 
 def debug_env():
     print(f"usr_agent: {USER_AGENT}");
     print(f"port: {PORT}");
 
 if __name__ == "__main__":
-    # debug_env()
-    # app.run(debug=True, port=PORT)
-    choose_article()
+    app.run(debug=True, port=PORT)
